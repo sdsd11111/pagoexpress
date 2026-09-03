1744d85b21d395517fc21dea5b825cab8e649ab7
Cesar Reyes <cesar@objetivocesar.com>
2026-09-03 10:25:49 -0500

    dsd

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST || "mysql.gb.stackcp.com",
-    port: Number(process.env.DB_PORT) || 41827,
-    user: process.env.DB_USER || "Pagoexpressprueba-353038396b0b",
-    password: process.env.DB_PASS || "hjc1u375jn",
-    database: process.env.DB_NAME || "Pagoexpressprueba-353038396b0b",
+    host: process.env.DB_HOST || "mysql.us.stackcp.com",
+    port: Number(process.env.DB_PORT) || 44641,
+    user: process.env.DB_USER || "dbpagoexpress-353039382b30",
+    password: process.env.DB_PASS || "tbP^CDIK£XEK",
+    database: process.env.DB_NAME || "dbpagoexpress-353039382b30",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
